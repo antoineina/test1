073f7af0f9df5f08eda663426ec9d693b3966e76
antoineina <40673309+antoineina@users.noreply.github.com>
2018-06-28 17:13:05 -0400

    Update Program.cs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -5,11 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApplication1
-{
+{    
     class Program
     {
         static void Main(string[] args)
         {
+            //Added a comment1
         }
     }
 }
